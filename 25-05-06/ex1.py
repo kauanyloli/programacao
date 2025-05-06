4769acb9ad3635cbb8b659967612b17d9e12a91d
kauanyloli <kauanyyuno@gmail.com>
2025-05-06 16:50:03 -0300

    1

---
+++ 25-05-06/ex1.py
@@ -0,0 +1,6 @@
+#faça um programa que solicite os valores de v0,a e t. o programa deve calcular e exibir a distancia pecorrida pelo objeto
+
+V0 = float(input('difite o valor de V0'))
+t = float(input('difite o valor de t'))
+A = float(input('difite o valor de A'))
+ = float(input('difite o valor de '))
