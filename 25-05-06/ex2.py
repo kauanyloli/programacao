fa16f934ccf7c2b87abe827cd63c8622b8595314
kauanyloli <kauanyyuno@gmail.com>
2025-05-06 17:13:09 -0300

    1

---
+++ 25-05-06/ex2.py
@@ -0,0 +1,8 @@
+#vc esta viajando de carro de mossoro para natal e,no momento, esta passando por lajes. o carroesta em movimento com uma velocidade inicial v0, e acelera constantimente ate atingir 
+#a velocidade final ao chegar em natal.A formula pra calcular o tempo de viagem t é dada por t= a.t2/2 + v0.t-d
+#solicita ao ussuaria as informaçoes:
+# Distancia (km)
+# velocidade inicial(em km/h)
+# aceleraçao do carro(em metros por segundo ao quadrado)
+#o programa deve calcular e exibir o tempo necessario para o carro pecorrer essa distancia ate natal, formatado no padrão hh:mm:ss.
+
