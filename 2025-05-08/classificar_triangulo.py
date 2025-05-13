6102f6bd9017b3fa95cf6d13442e39b63df1f915
kauanyloli <kauanyyuno@gmail.com>
2025-05-13 17:24:55 -0300

    1

---
+++ 2025-05-08/classificar_triangulo.py
@@ -0,0 +1,10 @@
+'''
+   Programa para classificar um triângulo quanto aos ângulos.
+
+   - O programa deverá solicitar 3 ângulos inteiros positivos;
+   - Para ser um triânguloa, a soma dos ângulos deve ser igual a 180;
+   
+   - Retângulo: Possui um ângulo interno reto (igual a 90).
+   - Obtusângulo: Possui um ângulo interno obtuso (maior que 90).
+   - Acutângulo: Possui todos os ângulos internos agudos (menores que 90).
+'''
