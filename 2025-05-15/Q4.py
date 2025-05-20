d3647766b9d64303af80a22fd418b0cbb4ec81e9
kauanyloli <kauanyyuno@gmail.com>
2025-05-20 17:37:50 -0300

    1

--- 2025-05-15/Q4.py
+++ 2025-05-15/Q4.py
@@ -26,3 +26,24 @@ delta igual a zero, a equação tem uma raiz real (ou duas iguais);
 delta menor que zero, a equação não tem raízes reais.
 
 Elabore um programa que solicite ao usuário os coeficientes da equação (a, b e c) e exiba as raízes reais da equação (quando houver).'''
+
+import math
+
+
+a=int(input(' informe o valor de A'))
+b=int(input(' informe o valor de B'))
+c=int(input(' informe o valor de C'))
+delta=b*b-4*a*c
+
+if delta>0:
+  x1= (-b+ math.sqrt(delta))/(2*a) 
+  x2= (-b- math.sqrt(delta))/(2*a) 
+  print(f' As raizes sao :',x1,x2)  
+if delta==0:
+    x=-b/(2*a)
+    print(f' A raiz é:',x)    
+    
+if delta<0:
+ print('Nao tem raizes')
+        
+    
