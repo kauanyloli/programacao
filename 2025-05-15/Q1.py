11b4554ace52e4d9d8d2f563f4436eef1a2da257
kauany <137922089+kauanyloli@users.noreply.github.com>
2025-05-24 22:26:01 -0300

    fiz

--- 2025-05-15/Q1.py
+++ 2025-05-15/Q1.py
@@ -3,13 +3,23 @@
 
 Exemplo: 2456 = 17 (2 + 4 + 5 + 6)
 '''
+#fiz
 import sys
+
 Num=int(input('Informe um número de até 4 dígitos inteiro positivo'))
-soma=0
-if Num>0:
-    resto = Num % 10
-    Num = (Num - resto)/10
-    soma = soma + resto
-print(f'O valor da soma é:',Num)   
-if Num<0:
-    sys.exit('ERRO... Por favor infomerme um numero valido')
+
+if Num < 0 or Num > 9999:
+        sys.exit('ERRO... Digite um numero valido')
+if Num > 0 or  Num < 9999:
+ mil = Num//1000
+ Num %= 1000  
+
+ cent = Num//100
+ Num %= 100  
+
+ dec = Num//10
+ Num %= 10   
+
+soma= mil+cent+dec+Num
+print(f'O valor da soma é:',soma)
+
