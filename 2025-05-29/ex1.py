e16c88191e9cd47696ca36d618111357ea54d719
kauanyloli <kauanyyuno@gmail.com>
2025-05-29 17:44:36 -0300

    ex1

--- 2025-05-29/ex1.py
+++ 2025-05-29/ex1.py
@@ -3,8 +3,23 @@ faça um programa que solicite ao usuario
 um numero inteiro e informe se ele é primo ou nao.'''
 import sys
 
-n=int(input('Digite um numero'))
-
-while n/2 :
-
-
+try:
+    n=int(input('Digite um numero para saber se é primo :'))
+    if n<=0:
+        print('este numero nao é primo')
+except ValueError:
+    sys.exit('ERRO...Digite um numero inteiro possitivo') 
+    
+except Exception as exc:
+    sys.exit(f'ERRO:{exc}')
+    
+else:
+    divisores = 1
+    contDivisor= 0
+    while divisores <=n:
+        if (n % divisores)==0:
+             contDivisor +=1         
+    if contDivisor == 2:
+        print(f'{n} é primo.')
+    else:
+        print(f'{n} não é primo.')
