5da19e32c58675f5e4b4ecae82268a0646248f45
kauanyloli <kauanyyuno@gmail.com>
2025-05-29 17:09:14 -0300

    ex

---
+++ 2025-05-29/ex1.py
@@ -0,0 +1,10 @@
+''' dado que um numero que so possuis dois divisores(1 e ele mesmo),
+faça um programa que solicite ao usuario
+um numero inteiro e informe se ele é primo ou nao.'''
+import sys
+
+n=int(input('Digite um numero'))
+
+while n/2 :
+
+
---
+++ 2025-05-29/ex2.py
@@ -0,0 +1,6 @@
+'''considerando que o fartorial de um numero é dado por:
+n! = n * (n-1)*(n-2)*(n-3)*....*1
+faça um programa que solicite um valor inteiro ao usuario e em seguida calcule
+e exiba o fatorial do numeroinformado.
+'''
+
