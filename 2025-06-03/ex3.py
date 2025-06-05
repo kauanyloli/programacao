8c9a2987b3124fa0136a9bc2432fda4d8a3ec71c
kauanyloli <kauanyyuno@gmail.com>
2025-06-05 17:19:46 -0300

    1

--- 2025-06-03/ex3.py
+++ 2025-06-03/ex3.py
@@ -6,3 +6,23 @@
    Implemente um programa que receba um número inteiro n e retorne os n 
    primeiros termos da sequência de Fibonacci.
 '''
+import sys
+try:
+    intNum = int(input('Digite a quantidade de elementos da sequencia:')) 
+except ValueError:
+   sys.exit('ERRRO: Digite um numero inteiro valido...')
+except Exception as e:
+          sys.exit('ERRRO{e}')
+else:
+    if intNum < 2:
+       sys.exit('ERRRO: a sequencia de ibonacci deve possuir pelo menos 2 numeros...')  
+
+intAtualNum = 1
+intProxNum = 1
+print(f'{intAtualNum},{intProxNum},',end='')
+
+if intNum ==2:
+     sys.exit('n')
+
+for i in range(3,intProxNum + 1):
+                    
