f8f29fc613375173ca8bc15db3f3af13bf8c7d93
kauany <kauanyyuno@gmail.com>
2025-07-07 22:24:48 -0300

    fiz

--- 2025-06-18/Q3.py
+++ 2025-06-18/Q3.py
@@ -3,5 +3,23 @@ de pontos que formam triângulos equiláteros em um plano (veja a definição de
 
 Faça um programa que solicite ao usuário um número inteiro positivo e informe se ele é (ou não) triangular, de acordo com a definição.'''
 
-import math
+try:
+    num = int(input("Digite um número inteiro positivo: "))
+    if num <= 0:
+        print("Por favor, insira um número positivo.")
+    else:
+        n = 1
+        triangular = False
 
+        while n * (n + 1) // 2 <= num:
+            if n * (n + 1) // 2 == num:
+                triangular = True
+                break
+            n += 1
+
+        if triangular:
+            print(f"O número {num} é triangular.")
+        else:
+            print(f"O número {num} não é triangular.")
+except:
+    print("Entrada inválida. Por favor, digite um número inteiro.")
--- 2025-06-18/Q7.py
+++ 2025-06-18/Q7.py
@@ -5,4 +5,57 @@ O programa deverá solicitar ao usuário as letras e à medida que as letras for
 sendo digitadas o programa irá exibir se o usuário acertou ou não. 
 O jogo deverá considerar maiúsculas e minúsculas iguais. O jogador poderá errar 6 vezes antes de ser enforcado.'''
 
-d
+try:
+    import random
+    palavras = ("Naruto ","Sasuke ","sakura","Gaara","itachi","deidara","kakashi","sasori","konan","pain","hidan",
+                "","","","")
+    # Escolhendo uma palavra aleatória!
+    segredo = palavras[random.randint(0,len(palavras)-1)]
+    # Convertendo todas as letras em MAIÚSCULAS!
+    segredo = segredo.upper()
+    # Contando quantidade de letras na palavra sorteada!
+    visivel = "_"*len(segredo)
+    # Limite de tentativas!
+    tentativas = 6
+
+    # Loop que permane até acertar todas as letras ou acabarem as tentativas!
+    while visivel != segredo and tentativas > 0 :
+        print(visivel)
+        print("Lhe restam: ",tentativas,"tentativas!")
+        # Lê uma letra e a converte em MAIÚSCULA!
+        letra = input("Digite uma letra: ")
+        letra = letra.upper()
+
+        # Não aceita mais de uma letra nem letras já em 'visivel'!
+        # Não altera 'segredo' e não reduz 'tentativas'!
+        if len(letra) != 1 or visivel.count(letra) !=0 :
+            print(letra,"Já foi digitado ou é mais que uma letra!") # Opcional
+        
+        else:
+            # Reduz uma tentativa caso erre a letra!
+            if segredo.count(letra) == 0:
+                tentativas = tentativas - 1
+        # Caixinha de comparação!
+        #######################################################
+            novavisivel = ""
+            for pos in range (len(segredo)):
+                # Caso de acerto de letra, troca "_" pela "letra" na visivel!
+                if letra == segredo[pos]:
+                    novavisivel += segredo[pos]
+                # Caso de erro de letra, mantem "visivel[pos]" na visivel!
+                else:
+                    novavisivel += visivel[pos]
+                    
+            # Sai do laço com uma nova visivel
+            visivel = novavisivel
+        #######################################################
+
+    # Exibe sucesso ou falha e exibe segredo!
+    if visivel == segredo:
+        print("Parabéns, você acertou!, a palavra era:",segredo)
+    else:
+        print("Esgotaram-se suas tentativas!, a palavra era:",segredo)
+
+    print("FIM") # Opcional
+except:
+    print("Em algum lugar, de alguma forma, algo deu errado e a culpa provavelmente foi sua!")
