af21ebc031f711a01add52f9c99c94d6acff8222
kauany <kauanyyuno@gmail.com>
2025-07-27 21:45:54 -0300

    1

--- 2025-07-20/q1.py
+++ 2025-07-20/q1.py
@@ -3,3 +3,33 @@ a quantidade de sub-listas da lista principal e o segundo indicando a quantidade
 cada sub-lista. Com base nesses valores, o programa deverá gerar aleatoriamente
 os elementos de cada sub-lista da lista principal (utilizar LIST COMPREHENSIONS),
 exibir a lista gerada e, em seguida, calcular e apresentar a lista transposta.'''
+
+import sys, random
+
+try:
+    elementos = int(input('Digite a quantidade de elementos de cada Sub-lista: '))
+    subListas = int(input('Digite a quantidade de Sub-listas: '))
+
+except ValueError:
+    sys.exit("\nERRO: informe um valor inteiro válido.")
+
+except Exception as excecao:
+    sys.exit(f"\nERRO: {excecao}")
+
+else:
+    lstPrincipal = list()
+    lstTransposta = list()
+    lstAux = list()
+
+    for _ in range(subListas):
+        lstSub = [random.randint(0,100) for _ in range(elementos)]
+        lstPrincipal.append(lstSub)
+
+    for sub in range(elementos):
+        lstAux = [lstPrincipal[elemento][sub] for elemento in range(subListas)]
+        lstTransposta.append(lstAux) 
+
+    print("\nLista Normal:")
+    for sub in lstPrincipal: print(sub)
+    print("\nLista Transposta:")
+    for transposta in lstTransposta: print(transposta)
--- 2025-07-20/q3.py
+++ 2025-07-20/q3.py
@@ -15,3 +15,35 @@ ATENÇÃO:
 NÃO USAR a biblioteca statistics.py para implementar o que foi pedido anteriormente;
 Para fins de conferência, o aluno poderá utilizar as funções mean(), median(), pvariance()
 e pstdev() da biblioteca statistics.py.'''
+
+import sys, random
+try:
+    elementos = int(input('Digite o valor de N elementos: '))
+    if elementos < 1:
+        sys.exit('\nERRO: informe um valor positivo e maior que 0.')
+except ValueError:
+    sys.exit('\nERRO: informe um valor inteiro válido.')
+except Exception as excecao:
+    sys.exit(f'\nERRO: {excecao}')
+else:
+    media = 0
+    mediana = 0 
+    somaMedias = 0
+    variancia = 0
+    lstNumeros = [random.randint(0, 99) for _ in range(elementos)]
+    lstNumeros.sort()
+    print(lstNumeros)
+
+    for numero in lstNumeros: media += numero
+    media = media / len(lstNumeros)
+    
+    if len(lstNumeros) % 2 == 0: mediana = (lstNumeros[(len(lstNumeros) // 2) - 1] + lstNumeros[(len(lstNumeros) // 2)]) / 2
+    else: mediana = lstNumeros[(len(lstNumeros) // 2)]
+
+    #Variancia e desvio!
+    lstVariancia = [(num - media) ** 2 for num in lstNumeros]
+    for numero in lstVariancia: somaMedias += numero
+    variancia = somaMedias / len(lstVariancia)
+    desvioPadrao = variancia ** (1/2)
+
+    print(f'Média = {media:.1f}; Mediana = {mediana}; Variância Populacional = {variancia}; Desvio Padrão = {desvioPadrao}')
